6f49dc17ed5a9bb5c159df57f80f643b7bc87f87
Dileep Kumar <dileep.pdkr@gmail.com>
2019-04-20 13:48:50 +0530

    background job changes

--- app/jobs/background_worker.rb
+++ app/jobs/background_worker.rb
@@ -2,7 +2,6 @@ module BackgroundWorker
 	class CreateReading
 	  include Sidekiq::Worker
 	  sidekiq_options retry: false
-
 	  def self.perform(params, reading_id)
 	    reading = Reading.new(:id => reading_id, :thermostat_id => params["thermostat_id"], :temperature => params["temperature"], :humidity => params["humidity"], :battery_charge => params["battery_charge"])
 	    reading.save!
